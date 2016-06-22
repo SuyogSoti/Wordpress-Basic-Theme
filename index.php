acbb6f38f79e1a2b9e606257d7a7adf83c7d6781
Suyog Soti <suyog.soti@gmail.com>
2016-06-21 19:28:13 -0600

    fixed index post presentation

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
   <div class="slideshow" id="home_slide"></div>
   <div class="wrapper" id="home_wrapper">
     <?php while ( have_posts() ) : ?>
-      <?php the_post(); ?>
-      <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
-      <?php the_excerpt(); ?>
+      <a href="<?php echo get_permalink(); ?>">
+        <div class="index_most_recent">
+          <?php the_post(); ?>
+          <h5><?php the_title(); ?></h5>
+          <span class="date"><?php the_date(); ?></span>
+          <?php the_excerpt(); ?>
+          <div class="author">
+            By: <?php the_author(); ?>
+          </div>
+        </div>
+      </a>
     <?php endwhile; ?>
   </div>
 <?php else: ?>
